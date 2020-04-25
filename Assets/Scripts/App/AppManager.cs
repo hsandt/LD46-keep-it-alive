bf22990d445494cf418592e3cde4caff016f4a74
Hyper Sonic <hs@fuwar.in>
2020-04-25 20:30:16 +0100

    [APP] Toggle fullscreen with F11

---
+++ Assets/Scripts/App/AppManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            Screen.fullScreen ^= true;
+        }
+    }
+}
